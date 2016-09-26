670a45ef99fbbd3e56453892460cdef4f2df5880
Bradley Falzon <brad@teambrad.net>
2016-09-26 21:18:08 +0930

    Remove comments from cmd/revgrep and useless print

--- cmd/revgrep/main.go
+++ cmd/revgrep/main.go
@@ -1,20 +1,12 @@
 package main
 
 import (
-	"fmt"
 	"os"
 
 	"github.com/bradleyfalzon/revgrep"
 )
 
 func main() {
-	fmt.Println("Starting...")
-
 	// Get lines changes
 	revgrep.Changes(nil, os.Stdin, os.Stderr)
-
-	// Open stdin and scan
-
-	// Check if line was affected
-
 }
