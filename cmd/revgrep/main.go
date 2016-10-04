a332317a12b75d2ead1794c72938e4a7c5a7bb75
Bradley Falzon <brad@teambrad.net>
2016-10-04 12:57:56 +1030

    RevisionFrom/To should be arguments not flags

--- cmd/revgrep/main.go
+++ cmd/revgrep/main.go
@@ -2,20 +2,28 @@ package main
 
 import (
 	"flag"
+	"fmt"
 	"os"
 
 	"github.com/bradleyfalzon/revgrep"
 )
 
 func main() {
-	debug := flag.Bool("d", false, "Debug")
-	revFrom := flag.String("from", "", "Filter issues to lines changed since (and including) this revision")
-	revTo := flag.String("to", "", "Filter issues to lines changed since (and including) this revision (requires -from to be set)")
+	flag.Usage = func() {
+		fmt.Println("Usage: revgrep [options] [from-rev] [to-rev]")
+		fmt.Println()
+		fmt.Println("from-rev filters issues to lines changed since (and including) this revision")
+		fmt.Println("  to-rev filters issues to lines changed since (and including) this revision, requires <from-rev>")
+		fmt.Println()
+		flag.PrintDefaults()
+	}
+
+	debug := flag.Bool("d", false, "Show debug output")
 	flag.Parse()
 
 	checker := revgrep.Checker{
-		RevisionFrom: *revFrom,
-		RevisionTo:   *revTo,
+		RevisionFrom: flag.Arg(0),
+		RevisionTo:   flag.Arg(1),
 	}
 
 	if *debug {
