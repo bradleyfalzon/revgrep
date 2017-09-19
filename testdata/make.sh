bradleyfalzon/revgrep

commit 8d3df443d0743c5b4346687e60ae485d75fbe3be

Remove diff tool requirement

revgrep.go
@@ -13,11 +13,14 @@ import (
 )
 
 type Checker struct {
-	// Unified patch file to read to detect lines being changed, if nil revgrep
+	// Patch file (unified) to read to detect lines being changed, if nil revgrep
 	// will attempt to detect the VCS and generate an appropriate patch. Auto
 	// detection will search for uncommitted changes first, if none found, will
 	// generate a patch from last committed change.
 	Patch io.Reader
+	// NewFiles is a list of file names (with absolute paths) where the entire
+	// contents of the file is new
+	NewFiles []string
 	// Debug sets the debug writer for additional output
 	Debug io.Writer
 }
@@ -35,7 +38,7 @@ func (c Checker) Check(reader io.Reader, writer io.Writer) int {
 	var writeAll bool
 	if c.Patch == nil {
 		var err error
-		c.Patch, err = GitPatch()
+		c.Patch, c.NewFiles, err = GitPatch()
 		if err != nil {
 			writeAll = true
 			c.debug("could not read git repo:", err)
@@ -79,9 +82,11 @@ func (c Checker) Check(reader io.Reader, writer io.Writer) int {
 			for _, fno := range fchanges {
 				if fno == lno {
 					changed = true
-					issueCount++
-					fmt.Fprintln(writer, scanner.Text())
 				}
+			}
+			if changed == true || fchanges == nil {
+				issueCount++
+				fmt.Fprintln(writer, scanner.Text())
 			}
 		}
 		if !changed {
@@ -113,6 +118,10 @@ func (c Checker) linesChanged() map[string][]uint64 {
 		s       state
 		changes = make(map[string][]uint64)
 	)
+
+	for _, file := range c.NewFiles {
+		changes[file] = nil
+	}
 
 	if c.Patch == nil {
 		return changes
@@ -161,46 +170,41 @@ func (c Checker) linesChanged() map[string][]uint64 {
 
 // GitPatch returns a patch from a git repository, if no git repository was
 // was found and no errors occurred, nil is returned, else an error is returned
-func GitPatch() (io.Reader, error) {
+func GitPatch() (io.Reader, []string, error) {
 	var patch bytes.Buffer
 
 	// check if git repo exists
 	if err := exec.Command("git", "status").Run(); err != nil {
 		// don't return an error, we assume the error is not repo exists
-		return nil, nil
+		return nil, nil, nil
 	}
-
-	var (
-		unstaged  bool
-		untracked bool
-	)
 
 	// make a patch for unstaged changes
 	// use --no-prefix to remove b/ given: +++ b/main.go
 	cmd := exec.Command("git", "diff", "--no-prefix")
 	cmd.Stdout = &patch
 	if err := cmd.Run(); err != nil {
-		return nil, fmt.Errorf("error executing git diff: %s", err)
+		return nil, nil, fmt.Errorf("error executing git diff: %s", err)
 	}
-	unstaged = patch.Len() > 0
+	unstaged := patch.Len() > 0
 
 	// make a patch for untracked files
+	var newFiles []string
 	ls, err := exec.Command("git", "ls-files", "-o").CombinedOutput()
 	if err != nil {
-		return nil, fmt.Errorf("error executing git ls-files: %s", err)
+		return nil, nil, fmt.Errorf("error executing git ls-files: %s", err)
 	}
 	for _, file := range bytes.Split(ls, []byte{'\n'}) {
 		if len(file) == 0 {
 			continue
 		}
-		makePatch(string(file), &patch)
-		untracked = true
+		newFiles = append(newFiles, string(file))
 	}
 
 	// If there's unstaged changes OR untracked changes (or both), then this is
 	// a suitable patch
-	if unstaged || untracked {
-		return &patch, nil
+	if unstaged || newFiles != nil {
+		return &patch, newFiles, nil
 	}
 
 	// check for changes in recent commit
@@ -208,18 +212,8 @@ func GitPatch() (io.Reader, error) {
 	cmd = exec.Command("git", "diff", "--no-prefix", "HEAD~")
 	cmd.Stdout = &patch
 	if err := cmd.Run(); err != nil {
-		return nil, fmt.Errorf("error executing git diff: %s", err)
+		return nil, nil, fmt.Errorf("error executing git diff: %s", err)
 	}
 
-	return &patch, nil
-}
-
-// makePatch makes a patch from a file on the file system, writes to patch
-// TODO this shouldn't require an external dependency and could be refactored
-// into a different method
-func makePatch(file string, patch io.Writer) {
-	cmd := exec.Command("diff", "-u", os.DevNull, file)
-	cmd.Stdout = patch
-	// ignore errors from cmd.Run(), this maybe an untracked due to binary file
-	cmd.Run()
+	return &patch, nil, nil
 }

revgrep_test.go
@@ -69,7 +69,7 @@ func TestGitPatch(t *testing.T) {
 
 	// Test for unstaged changes
 
-	patch, err := GitPatch()
+	patch, newfiles, err := GitPatch()
 	if err != nil {
 		t.Fatalf("unexpected error from git: %v", err)
 	}
@@ -91,7 +91,7 @@ func TestGitPatch(t *testing.T) {
 
 	// Test for last commit
 
-	patch, err = GitPatch()
+	patch, _, err = GitPatch()
 	if err != nil {
 		t.Fatalf("unexpected error from git: %v", err)
 	}
@@ -110,12 +110,15 @@ func TestGitPatch(t *testing.T) {
 
 	// Test for handling non git dir
 
-	patch, err = GitPatch()
+	patch, newfiles, err = GitPatch()
 	if err != nil {
 		t.Fatalf("unexpected error from git: %v", err)
 	}
 	if patch != nil {
 		t.Fatalf("expected nil, got %v", patch)
+	}
+	if newfiles != nil {
+		t.Fatalf("expected nil, got %v", newfiles)
 	}
 
 }
@@ -124,17 +127,13 @@ func TestGitPatchUntracked(t *testing.T) {
 	prevwd := setup(t)
 	defer teardown(t, prevwd)
 
-	exp := []byte(`+var _ = fmt.Sprintf("untracked %v") // untracked.go:2: missing argument for Sprintf("untracked %v")...`)
-
 	// Test for unstaged changes
 
-	patch, err := GitPatch()
+	_, newfiles, err := GitPatch()
 	if err != nil {
 		t.Fatalf("unexpected error from git: %v", err)
 	}
-	if !bytes.Contains(patch.(*bytes.Buffer).Bytes(), exp) {
-		t.Fatalf("GitPatch did not detect untracked changes\n%s\ndoes not contain: %s",
-			patch.(*bytes.Buffer).Bytes(), exp,
-		)
+	if len(newfiles) != 1 || newfiles[0] != "untracked.go" {
+		t.Fatalf("GitPatch did not detect untracked.go, got: %#v", newfiles)
 	}
 }

commit 6776d38947acc69b33201877e07f2672cfff5264

Fix bug where file with only removed lines was being detected as new
Checker.linesChanged returns a map of files and a slice of positions
that have changed. If the slice was nil, it was an indication that
the file was new, so any issues found in that file were new and should
be raised. But if the file only had lines that were removed (not added
or modified), the slice would also be new.

This change introduces a third state, where an empty slice would indicate
no lines have been added (or modified).

I don't think this is an ideal change, the bug was likely introduced
in 8d3df443d0743c5b4346687e60ae485d75fbe3be, where previous to that
new files had an artificial diff created indicating all lines were
added. That commit was designed to remove the external dependency in
GNU diff.

I've settled on this change simply because it's straight forward and
doesn't add another dependency, but I'm not convinced it's the best
change either.

There's other related issues to this, some tools may want to see issues
on lines that have been removed, such as apicompat, a larger change
may want to take this into account.

Related to https://github.com/bradleyfalzon/gopherci/issues/86.
Related to https://github.com/bradleyfalzon/gopherci/issues/63.

revgrep.go
@@ -207,7 +207,9 @@ type pos struct {
 	hunkPos int // position relative to first @@ in file
 }
 
-// linesChanges returns a map of file names to line numbers being changed
+// linesChanges returns a map of file names to line numbers being changed.
+// If key is nil, the file has been recently added, else it contains a slice
+// of positions that have been added.
 func (c Checker) linesChanged() map[string][]pos {
 	type state struct {
 		file    string
@@ -242,7 +244,7 @@ func (c Checker) linesChanged() map[string][]pos {
 				changes[s.file] = s.changes
 			}
 			// 6 removes "+++ b/"
-			s = state{file: line[6:], hunkPos: -1}
+			s = state{file: line[6:], hunkPos: -1, changes: []pos{}}
 		case strings.HasPrefix(line, "@@ "):
 			//      @@ -1 +2,4 @@
 			// chdr ^^^^^^^^^^^^^

revgrep_test.go
@@ -103,6 +103,8 @@ func TestChangesWriter(t *testing.T) {
 		"10-committed": {"", []string{"main.go:6:"}, "HEAD~1", "HEAD~0"},
 		// static analysis tools with absolute paths should be handled
 		"11-abs-path": {"", []string{"main.go:6:"}, "HEAD~1", "HEAD~0"},
+		// Removing a single line shouldn't raise any issues.
+		"12-removed-lines": {"", nil, "", ""},
 	}
 
 	for stage, test := range tests {

testdata/make.sh
@@ -102,3 +102,28 @@ if [[ "$1" == "11-abs-path" ]]; then
     go vet |& sed -r "s:(.*\.go):$(pwd)/\1:g"
     exit
 fi
+
+# Remove one line on a file with existing issues
+
+if [[ "$1" == "12-removed-lines" ]]; then
+
+    cat > 12-removed-lines.go <<EOF
+package main
+import "fmt"
+var _ = fmt.Sprintf("12-removed-lines %s")
+// some comment that will be removed
+EOF
+
+    git add .
+    git commit -m "Commit" > /dev/null
+
+    cat > 12-removed-lines.go <<EOF
+package main
+import "fmt"
+var _ = fmt.Sprintf("12-removed-lines %s")
+EOF
+
+    git add .
+    git commit -m "Commit" > /dev/null
+    close
+fi